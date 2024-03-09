6290912950742431b16ed9f8a10e4a77f11e2d55
ZakhTania <tatizarova@gmail.com>
2024-03-09 12:20:50 +0300

    fix Playlist export

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,7 +3,8 @@ import playlist02 from "public/img/playlist02.png";
 import playlist03 from "public/img/playlist03.png";
 import styles from "./Sidebar.module.css";
 import { Personal } from "@/components/Personal";
-import Playlist from "@/components/Playlist/Playlist";
+import { Playlist } from "@/components/Playlist";
+
 
 export default function Sidebar() {
   return (
@@ -11,9 +12,9 @@ export default function Sidebar() {
       <Personal />
       <div className={styles.block}>
         <div className={styles.list}>
-            <Playlist src={playlist01} alt="day's playlist" />
-            <Playlist src={playlist02} alt="one hundred dance hits" />
-            <Playlist src={playlist03} alt="indie" />
+          <Playlist src={playlist01} alt="day's playlist" />
+          <Playlist src={playlist02} alt="one hundred dance hits" />
+          <Playlist src={playlist03} alt="indie" />
         </div>
       </div>
     </div>
